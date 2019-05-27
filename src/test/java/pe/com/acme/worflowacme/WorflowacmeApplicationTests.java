5c18ab4ef1df2d3bfae8a3e2ef7ccddb1a42540d
Ivan Leiva <leivagarcia18@gmail.com>
2019-05-27 00:02:47 -0500

    adding testing profile

--- src/test/java/pe/com/acme/worflowacme/WorflowacmeApplicationTests.java
+++ src/test/java/pe/com/acme/worflowacme/WorflowacmeApplicationTests.java
@@ -3,10 +3,12 @@ package pe.com.acme.worflowacme;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class WorflowacmeApplicationTests {
 
 	@Test
